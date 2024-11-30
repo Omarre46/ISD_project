79e06ee5091f301c12a0cbc70793cd4997d52cff
Hasan-Itani <itaniHasan64@gmail.com>
2024-11-30 15:06:31 +0200

    test

--- client_side/home.php
+++ client_side/home.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style/home.css">
+    <link rel="stylesheet" href="../includes/style/navbar.css">
+    <link rel="stylesheet" href="../includes/style/footer.css">
 </head>
 
 <body>
