08b837f0d06549af722c26ac063b5da5b61e5327
Omarre46 <12132034@students.liu.edu.lb>
2024-12-03 14:24:54 +0200

    navbar css modificatiob

--- includes/navbar.php
+++ includes/navbar.php
@@ -10,6 +10,20 @@
 
 </head>
 
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+        const links = document.querySelectorAll("#links a");
+        const currentPage = window.location.pathname.split("/").pop();
+
+        links.forEach(link => {
+            if (link.getAttribute("href") === currentPage) {
+                link.classList.add("active");
+            }
+        });
+    });
+</script>
+
+
 <body>
 
     <div class="navbar_container">
