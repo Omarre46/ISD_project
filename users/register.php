43daa80643daabe5d01219ce2204727503d3a8b8
Omarre46 <12132034@students.liu.edu.lb>
2024-12-12 12:28:36 +0200

    php modifictaion

--- users/register.php
+++ users/register.php
@@ -94,7 +94,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </div>
 
                  
-                <span><?php echo $error; ?></span>
+                <span style="color: red;font-size:larger"><?php echo $error; ?></span>
                 <a href="#"><input type="submit" name="submit" value="Register"></a>
               
             </form>
